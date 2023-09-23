0fd51a59cb634971c6fd6e8184742bd03a9d4d6d
Anamgulzar67 <69452477+Anamgulzar67@users.noreply.github.com>
2023-09-23 20:03:15 +0500

    completed

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,2 +1,60 @@
-console.log("hello");
-export {};
+import * as inquirer from "inquirer";
+import chalk from "chalk";
+// calculator operators
+var Operators;
+(function (Operators) {
+    Operators["ADD"] = "Addition";
+    Operators["SUBTRACT"] = "Subtraction";
+    Operators["MULTIPLY"] = "Multiplication";
+    Operators["DEVIDE"] = "Division";
+})(Operators || (Operators = {}));
+const prompt = inquirer.createPromptModule();
+// function validateNumber(input: string): boolean | string {
+//   if (isNaN(parseFloat(input))) {
+//     return "Please enter a valid number";
+//   }
+//   return true;
+// }
+async function main() {
+    const input = await prompt([
+        {
+            type: "input",
+            name: "num1",
+            message: "Enter the first number",
+            // validate: validateNumber,
+        },
+        {
+            type: "list",
+            name: "operator",
+            message: "select operation:",
+            choices: Object.values(Operators)
+        },
+        {
+            type: "input",
+            name: "num2",
+            message: "Enter the second number",
+            // validate:validateNumber,
+        }
+    ]);
+    const num1 = parseFloat(input.num1);
+    const num2 = parseFloat(input.num2);
+    const selectedOperator = input.operator;
+    let result;
+    if (selectedOperator === Operators.ADD) {
+        result = num1 + num2;
+        console.log(chalk.green.bgRedBright(`result is :${result}`));
+    }
+    else if (selectedOperator === Operators.MULTIPLY) {
+        result = num1 * num2;
+        console.log(chalk.green.bgBlueBright(`result is :${result}`));
+    }
+    else if (selectedOperator === Operators.SUBTRACT) {
+        result = num1 - num2;
+        console.log(chalk.green.bgCyanBright(`result is :${result}`));
+    }
+    else if (selectedOperator === Operators.DEVIDE) {
+        result = num1 / num2;
+        console.log(chalk.green.bgCyanBright(`result is :${result}`));
+    }
+}
+main();
